4f2997715afe2f95679623e3f506e74b01afe29d
Muhammad Rizki <85568859+faizrizki@users.noreply.github.com>
2024-05-14 13:09:19 +0700

    Add files via upload

---
+++ transaksiPC/transaksiPC/transaksiPC/pc.cs
@@ -0,0 +1,9 @@
+﻿namespace ReservationSystem.Models
+{
+    public class PC
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsReserved { get; set; }
+    }
+}
